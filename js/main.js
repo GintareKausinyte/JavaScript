9ab7bd97f30845f08951488e6628c7f7d3384fe0
GintareKausinyte <gintare.kausinyte@gmail.com>
2020-05-11 05:01:13 +0300

    Table sorting by number added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 const table = document.querySelector('.table');
-const nullTable = document.querySelector('.js-container')
-const title=document.querySelector('.title');
-let sortDirection=false;
+const title = document.querySelector('.title');
+let isAccending=true;
+
+
 
 loadTable(bookData);
 
-function loadTable(bookData){
-if (bookData.length != 0) {
-title.innerHTML=`            
+function loadTable(bookData) {
+    if (bookData.length != 0) {
+        title.innerHTML = `            
 <img src="img/580b585b2edbce24c47b2772.png">
 <h1>Knygos</h1>`
-table.innerHTML=`
+        table.innerHTML = `
 <div class="table-header">
                 <span>Pavadinimas</span>
                 <span>Autorius</span>
@@ -19,10 +20,8 @@ table.innerHTML=`
             </div>
             <hr>
 `
-
-
-    bookData.forEach(books => {
-        table.innerHTML += `  
+        bookData.forEach(books => {
+            table.innerHTML += `  
 <div class="table-data">
 <span>${books.title}</span>
 <span>${books.author}</span>
@@ -30,14 +29,41 @@ table.innerHTML=`
 <span>${books.year}</span>
 </div>
 `
-    });
-} else {
-    nullTable.innerHTML = `
+        });
+
+        const yearHeader = table.querySelector("span:nth-of-type(4)");
+        console.log(yearHeader);
+    
+        yearHeader.addEventListener("click", sortDir);
+        console.log(isAccending);
+        switch(isAccending){
+            case true:
+                bookData.sort((a, b)=>a.year-b.year);
+                yearHeader.classList.add('desc');
+                break;
+            case false:
+                bookData.sort((a, b)=> b.year-a.year);
+                yearHeader.classList.add('asc');
+                break;
+        }
+        
+    } else {
+        nullTable.innerHTML = `
   
     <div class="no-data"><span>Knygų sąrašas tuščias<span></div>
     `
+    }
+
 }
 
+
+function sortDir(){
+    if(isAccending==true){
+        isAccending=false;
+    }else{
+        isAccending=true;
+    }
+    loadTable(bookData);
 }
 
 
